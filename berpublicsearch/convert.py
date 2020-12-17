b2b960551e9e407ce6fe846946dd6e1883038086
Rowan Molony <rowan.molony@codema.ie>
2020-12-17 10:23:39 +0000

    Replace prefect flow with individual functions (#16)
    * Replace prefect flow with individual functions
    
    Can now download or re-download the BERPublicsearch
    separate to the conversion and unzipping
    
    * Add read raw text file function
    
    If conversion to parquet fails can resort to
    read txt file to dask function which runs slower

--- berpublicsearch/convert.py
+++ berpublicsearch/convert.py
@@ -1,29 +1,22 @@
 import csv
 import json
 from shutil import unpack_archive
-from shutil import rmtree
 
 from os import path
 from pathlib import Path
 
 import dask.dataframe as dd
-import prefect
-from prefect.triggers import always_run
-
-from prefect import task
 
 
 HERE = Path(__file__).parent
 
 
-@task
 def unzip(input_filepath: str, output_filepath: str) -> None:
 
     unpack_archive(input_filepath, output_filepath)
 
 
-@task
-def convert(input_dirpath: str, output_filepath: str) -> None:
+def convert_to_parquet(input_dirpath: str, output_filepath: str) -> None:
     """Convert csv file to parquet.
 
     Args:
@@ -39,15 +32,7 @@ def convert(input_dirpath: str, output_filepath: str) -> None:
         low_memory=False,
         dtype=dtypes,
         encoding="latin-1",
-        lineterminator="\n",
-        error_bad_lines=False,
         quoting=csv.QUOTE_NONE,
     )
 
-    ber_raw.to_parquet(output_filepath, schema="infer")
-
-
-@task(trigger=always_run)
-def delete_folder(filepath: str) -> None:
-
-    rmtree(filepath)
+    ber_raw.to_parquet(output_filepath, engine="pyarrow")
