062d8a337119d911de9f6e5052aed4744f96a81f
Rowan Molony <rowan.molony@codema.ie>
2021-06-15 09:35:24 +0100

    Calc vol, fab & vent heat loss ind of hlp
    ... rather than having one giant Heat Loss Parameter function

--- tests/deap/test_fab.py
+++ tests/deap/test_fab.py
@@ -40,40 +40,16 @@ def test_calculate_fabric_heat_loss():
     assert_series_equal(rounded_output, expected_output)
 
 
-def test_calculate_heat_loss_parameter(
-    building_area,
-    building_fabric,
-    building_floor_dimensions,
-):
+def test_calculate_heat_loss_parameter():
     """Output is equivalent to DEAP 4.2.0 example A"""
-    floor_area, roof_area, wall_area, window_area, door_area = building_area
-    (
-        floor_uvalue,
-        roof_uvalue,
-        wall_uvalue,
-        window_uvalue,
-        door_uvalue,
-        thermal_bridging_factor,
-        effective_air_rate_change,
-    ) = building_fabric
-    total_floor_area = pd.Series([126])
-    building_volume = pd.Series([321])
-    expected_output = pd.Series([0.96], dtype="float64")
+    fabric_heat_loss = pd.Series([0.5])
+    ventilation_heat_loss = pd.Series([0.5])
+    total_floor_area = pd.Series([1])
+    expected_output = pd.Series([1], dtype="float64")
 
     output = fab.calculate_heat_loss_parameter(
-        building_volume=building_volume,
-        roof_area=roof_area,
-        roof_uvalue=roof_uvalue,
-        wall_area=wall_area,
-        wall_uvalue=wall_uvalue,
-        floor_area=floor_area,
-        floor_uvalue=floor_uvalue,
-        window_area=window_area,
-        window_uvalue=window_uvalue,
-        door_area=door_area,
-        door_uvalue=door_uvalue,
-        thermal_bridging_factor=thermal_bridging_factor,
-        effective_air_rate_change=effective_air_rate_change,
+        fabric_heat_loss=fabric_heat_loss,
+        ventilation_heat_loss=ventilation_heat_loss,
         total_floor_area=total_floor_area,
     )
 
