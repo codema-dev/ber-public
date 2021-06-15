cd3bcd112a6f0b973b1976c18e4cb9c3bdf87cd8
Rowan Molony <rowan.molony@codema.ie>
2021-06-15 14:04:05 +0100

    Refactor building_volume out of fab calcs
    Instead calculate it separately and pass it as an argument to these
    functions

--- tests/deap/test_fab.py
+++ tests/deap/test_fab.py
@@ -4,18 +4,19 @@ from pandas.testing import assert_series_equal
 from ber_public.deap import fab
 
 
-def test_calculate_fabric_heat_loss(building_area, building_fabric):
+def test_calculate_fabric_heat_loss():
     """Output is equivalent to DEAP 4.2.0 example A"""
-    floor_area, roof_area, wall_area, window_area, door_area = building_area
-    (
-        floor_uvalue,
-        roof_uvalue,
-        wall_uvalue,
-        window_uvalue,
-        door_uvalue,
-        thermal_bridging_factor,
-        *_,
-    ) = building_fabric
+    floor_area = pd.Series([63])
+    roof_area = pd.Series([63])
+    wall_area = pd.Series([85.7])
+    window_area = pd.Series([29.6])
+    door_area = pd.Series([1.85])
+    floor_uvalue = pd.Series([0.14])
+    roof_uvalue = pd.Series([0.11])
+    wall_uvalue = pd.Series([0.13])
+    window_uvalue = pd.Series([0.87])
+    door_uvalue = pd.Series([1.5])
+    thermal_bridging_factor = pd.Series([0.05])
 
     expected_output = pd.Series([68], dtype="int64")
 
@@ -41,7 +42,6 @@ def test_calculate_heat_loss_parameter(
     building_area,
     building_fabric,
     building_floor_dimensions,
-    building_floor_dimensions_approx,
 ):
     """Output is equivalent to DEAP 4.2.0 example A"""
     floor_area, roof_area, wall_area, window_area, door_area = building_area
@@ -54,27 +54,12 @@ def test_calculate_heat_loss_parameter(
         thermal_bridging_factor,
         effective_air_rate_change,
     ) = building_fabric
-    (
-        ground_floor_area,
-        ground_floor_height,
-        first_floor_area,
-        first_floor_height,
-        second_floor_area,
-        second_floor_height,
-        third_floor_area,
-        third_floor_height,
-    ) = building_floor_dimensions
-    total_floor_area = (
-        ground_floor_area
-        + first_floor_area.fillna(0)
-        + second_floor_area.fillna(0)
-        + third_floor_area.fillna(0)
-    )
-    no_of_storeys, assumed_floor_height = building_floor_dimensions_approx
-
+    total_floor_area = pd.Series([126])
+    building_volume = pd.Series([321])
     expected_output = pd.Series([0.96], dtype="float64")
 
     output = fab.calculate_heat_loss_parameter(
+        building_volume=building_volume,
         roof_area=roof_area,
         roof_uvalue=roof_uvalue,
         wall_area=wall_area,
@@ -85,20 +70,9 @@ def test_calculate_heat_loss_parameter(
         window_uvalue=window_uvalue,
         door_area=door_area,
         door_uvalue=door_uvalue,
-        total_floor_area=total_floor_area,
         thermal_bridging_factor=thermal_bridging_factor,
         effective_air_rate_change=effective_air_rate_change,
-        ground_floor_area=ground_floor_area,
-        ground_floor_height=ground_floor_height,
-        first_floor_area=first_floor_area,
-        first_floor_height=first_floor_height,
-        second_floor_area=second_floor_area,
-        second_floor_height=second_floor_height,
-        third_floor_area=third_floor_area,
-        third_floor_height=third_floor_height,
-        no_of_storeys=no_of_storeys,
-        assumed_floor_height=assumed_floor_height,
+        total_floor_area=total_floor_area,
     )
-    rounded_output = output.round(2).astype("float64")
 
-    assert_series_equal(rounded_output, expected_output)
+    assert_series_equal(output.round(2), expected_output)
